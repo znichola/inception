f4daf85e04475658b091d429876aa3caa391ce6b
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-08-24 22:24:18 +0200

    entrypoint rework

--- srcs/requirements/bonus/svelt/script/entrypoint.sh
+++ srcs/requirements/bonus/svelt/script/entrypoint.sh
@@ -8,6 +8,12 @@ log "entrypointing into node\n"
 
 if [ $1 == "tail" ]; then
 	log " doing infinate loop tail\n"
+	log " empty folder so we need to do the copy bullshit"
+
+	cp /sveltapp-backup /root/sveltapp2
+	cd /root/sveltapp2
+	npm install
+
 else
 
 	log "done sleeping handing over the process to what came before\n"
