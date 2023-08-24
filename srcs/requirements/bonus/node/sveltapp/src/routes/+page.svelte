6ee62b70a4d8f39cc76b85956bf09a906f00c32b
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-08-24 22:12:29 +0200

    svelt app start

---
+++ srcs/requirements/bonus/node/sveltapp/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 style="text-align: center; line-height: 100px;">What does it mean to be..</h1>
+<p style="text-align: center; line-height: 1.5;">
+Who cares? Now where's that spinning top, I still can't figure out what container I'm in.</p>
+
+
+<footer style="position: absolute; bottom: 0; left: 0; right: 0; padding: 20px; text-align: center; font-weight: 300;">
+  Who knows really, but visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.
+</footer>
+
